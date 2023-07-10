5f95d4420af5c1ece3d495cd24066c66418c5bbf
Dylan Foster <dylantf@gmail.com>
2023-07-10 10:15:28 -0600

    Add context fn for marking down price of a product

--- lib/pento/catalog/product.ex
+++ lib/pento/catalog/product.ex
@@ -11,12 +11,18 @@ defmodule Pento.Catalog.Product do
     timestamps()
   end
 
-  @doc false
   def changeset(product, attrs) do
     product
     |> cast(attrs, [:name, :description, :unit_price, :sku])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
     |> validate_number(:unit_price, greater_than: 0.0)
+  end
+
+  def changeset_markdown_price(product, attrs) do
+    product
+    |> cast(attrs, [:unit_price])
+    |> validate_required([:unit_price])
+    |> validate_number(:unit_price, less_than: product.unit_price)
   end
 end
