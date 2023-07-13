3e8c3e923a8d401e20c898571778164ff68c8a2d
Dylan Foster <dylantf@gmail.com>
2023-07-12 19:37:38 -0600

    Add product ratings. Finish part II

---
+++ lib/pento_web/live/rating_live/form.ex
@@ -0,0 +1,42 @@
+defmodule PentoWeb.RatingLive.Form do
+  use PentoWeb, :live_component
+  alias Pento.Survey
+  alias Pento.Survey.Rating
+
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_rating()
+     |> assign_changeset()}
+  end
+
+  defp assign_rating(%{assigns: %{current_user: user, product: product}} = socket) do
+    assign(socket, :rating, %Rating{user_id: user.id, product_id: product.id})
+  end
+
+  defp assign_changeset(%{assigns: %{rating: rating}} = socket) do
+    assign(socket, :changeset, Survey.change_rating(rating))
+  end
+
+  def handle_event("save", %{"rating" => rating_params}, socket) do
+    {:noreply, save_rating(socket, rating_params)}
+  end
+
+  def save_rating(
+        %{assigns: %{product_index: product_index, product: product}} = socket,
+        rating_params
+      ) do
+    IO.inspect(rating_params, label: "Rating params")
+
+    case Survey.create_rating(rating_params) do
+      {:ok, rating} ->
+        product = %{product | ratings: [rating]}
+        send(self(), {:created_rating, product, product_index})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign(socket, changeset: changeset)
+    end
+  end
+end
