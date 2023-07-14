2330df31497b472b0b2d60fcb46177e6dfc8451f
Dylan Foster <dylantf@gmail.com>
2023-07-14 16:46:20 -0600

    Start adding tests

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -24,21 +24,21 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
      |> assign_chart_svg()}
   end
 
-  defp assign_age_group_filter(socket, age_group_filter) do
+  def assign_age_group_filter(socket, age_group_filter) do
     assign(socket, :age_group_filter, age_group_filter)
   end
 
-  defp assign_age_group_filter(%{assigns: %{age_group_filter: age_group_filter}} = socket) do
+  def assign_age_group_filter(%{assigns: %{age_group_filter: age_group_filter}} = socket) do
     assign(socket, :age_group_filter, age_group_filter)
   end
 
-  defp assign_age_group_filter(socket) do
+  def assign_age_group_filter(socket) do
     assign(socket, :age_group_filter, "all")
   end
 
-  defp assign_products_with_average_ratings(
-         %{assigns: %{age_group_filter: age_group_filter}} = socket
-       ) do
+  def assign_products_with_average_ratings(
+        %{assigns: %{age_group_filter: age_group_filter}} = socket
+      ) do
     socket
     |> assign(
       :products_with_average_ratings,
